09685aadf77364bf3b494f8031b37ba15d3265d0
timothy-r <tim.rodger@gmail.com>
2013-05-12 13:06:15 +0100

    added Node constructor

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-    Node n,l,r;
+    Node n(40);
+    Node l(10);
+    Node r(45);
+
     n.setLeft(&l);
     n.setRight(&r);
     return 0;
--- node.cpp
+++ node.cpp
@@ -7,9 +7,15 @@ class Node
         int value;
     
     public:
+        Node(int value);
         void setLeft(Node *n);
         void setRight(Node *n);
 };
+
+Node::Node(int v)
+{
+    value = v;
+}
 
 void Node::setLeft(Node *n)
 {
